cfcb4a71ac02a41504ae3f95d7215f6a6e34642e
LucasMoncadaCL <lucasmoncada017@gmail.com>
2025-06-13 01:37:14 -0400

    apis publicas ya puestas bien como publicas, haciendo que las rutas esten en el securityConfig. Tambien ambie el nombre de la base de datos para que coincida con el de la otras apis.

--- src/main/java/com/gateway/jwt/security/SecurityConfig.java
+++ src/main/java/com/gateway/jwt/security/SecurityConfig.java
@@ -8,6 +8,17 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.gateway.redireccion.carritos.CarritoPublicRoutes;
+import com.gateway.redireccion.comentarios.ComentariosPublicRoutes;
+import com.gateway.redireccion.cupones.CuponesPublicRoutes;
+import com.gateway.redireccion.cursos.CursosPublicRoutes;
+import com.gateway.redireccion.pagos.PagosPublicRoutes;
+import com.gateway.redireccion.roles.RolesPublicRoutes;
+import com.gateway.redireccion.soporte.SoportePublicRoutes;
+import com.gateway.redireccion.ubicacion.UbicacionPublicRoutes;
+import com.gateway.redireccion.usuarios.UsuariosPublicRoutes;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.http.HttpMethod; // Import necesario
@@ -26,6 +37,22 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(HttpMethod.POST, PUBLIC_POST).permitAll() // Rutas públicas POST
                 .requestMatchers(HttpMethod.GET, PUBLIC_GET).permitAll()   // Rutas públicas GET
+                .requestMatchers(HttpMethod.GET, RolesPublicRoutes.ROLES_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.GET, CuponesPublicRoutes.CUPONES_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.GET, UsuariosPublicRoutes.USUARIOS_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.POST, UsuariosPublicRoutes.USUARIOS_PUBLIC_POST).permitAll()
+                .requestMatchers(HttpMethod.GET, CarritoPublicRoutes.CARRITOS_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.POST, CarritoPublicRoutes.CARRITOS_PUBLIC_POST).permitAll()
+                .requestMatchers(HttpMethod.GET, ComentariosPublicRoutes.COMENTARIOS_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.POST, ComentariosPublicRoutes.COMENTARIOS_PUBLIC_POST).permitAll()
+                .requestMatchers(HttpMethod.GET, CursosPublicRoutes.CURSOS_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.POST, CursosPublicRoutes.CURSOS_PUBLIC_POST).permitAll()
+                .requestMatchers(HttpMethod.GET, PagosPublicRoutes.PAGOS_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.POST, PagosPublicRoutes.PAGOS_PUBLIC_POST).permitAll()
+                .requestMatchers(HttpMethod.GET, SoportePublicRoutes.SOPORTE_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.POST, SoportePublicRoutes.SOPORTE_PUBLIC_POST).permitAll()
+                .requestMatchers(HttpMethod.GET, UbicacionPublicRoutes.UBICACION_PUBLIC_GET).permitAll()
+                .requestMatchers(HttpMethod.POST, UbicacionPublicRoutes.UBICACION_PUBLIC_POST).permitAll()
                 .anyRequest().authenticated() // Todas las demás requieren autenticación
             )
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
